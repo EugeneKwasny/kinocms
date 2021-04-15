1a1b42ee0993807a026aeadc563021b63c93b1e0
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2021-04-15 16:59:27 +0300

    Added: films index template markup

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -1,5 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <section class="container">
+        <aside class="site-sidebar">
+            <div class="widget sidebar-menu">
+                <ul>
+                    <li class="active">
+                        <a href="#">Афиша</a>
+                    </li>
+                    <li>
+                        <a href="#">Скоро</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="widget adblock">
+                <a href="#">
+                    <img scr="{{asset('img/banner.png')}}"
+                </a>
+            </div>
+        </aside>
+        <section class="items-grid">
+            <ul>
+                @for ($i=0;$i<18;$i++)
+                    <li class="grid-item">
+                        <a href="#" class="grid-item__thumbnail">
+                            <img src="{{ asset('img/grid-cover.png') }}">
+                            <span class="badge">3d</span>
+                            <span class="badge">DBOX</span>
+                            <span class="badge">12+</span>
+                        </a>
+                        <a href="#" class="grid-item__heading">
+                            Иллюзия обмана
+                        </a>
+                        <a href="#" class="grid-item__cta">Купить</a>
+                    </li>      
+                @endfor
+            </ul>
+        </section>
+    </section>
 @endsection

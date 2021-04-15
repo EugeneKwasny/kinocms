e43b4d454e0ebefee3a773c03fbd95b6e3e6a27c
Eugene Kvasnyi <e.kvasnyi@gmail.com>
2021-04-15 17:20:08 +0300

    Added: hero slider section markup

--- resources/views/films/show.blade.php
+++ resources/views/films/show.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+    <section class="hero-slider">
+        <ul>
+            @for ($i=0;$i<3;$++)
+                <li>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/tI1JGPhYBS8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </li>
+            @endfor
+        </ul>
+    </section>
 
 @endsection
